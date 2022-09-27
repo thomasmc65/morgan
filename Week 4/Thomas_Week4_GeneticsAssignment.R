3ed12229d206434b5198ce5cb22037913e4f87d2
thomasmc65 <112492397+thomasmc65@users.noreply.github.com>
2022-09-26 23:39:34 -0400

    week 4

--- Week 4/Thomas_Week4_GeneticsAssignment.R
+++ Week 4/Thomas_Week4_GeneticsAssignment.R
@@ -1,11 +1,11 @@
 # Look at the plot and model results for our Dryad data in the tutorial. Part 1: Without knowing which points represent which groups, 
   # Give one explanation for why these data might be difficult to draw spatial inferences about genes.(3 points)
 
-#These data might be difficult to use to draw spatial inferences about genes because 
+#These data might be difficult to use to draw spatial inferences about genes because many of them are clustered together on the plot. This makes it difficult to understand relationships between the different regions.
 
   # Part 2: Despite the drawbacks, give the result or interpretation that you feel most confident in (3 points), and EXPLAIN WHY (4 points).
 
-#
+#The grey points are likely the most correlated as they are clustered closest together and have the fewest outliers compared to the data points of other regions.
 
 # For your scripting assignment we will use the "ge_data" data frame found in the "stability" package.
   # Install the "stability" package, load it into your R environment, and use the data() function to load the "ge_data". (2 points)
@@ -39,4 +39,4 @@ summary(mod.gen)
 
 # Which environment would be your very WORST choice for generating a strong yield response? (2 points)
 
-#The environment that would be the worst choice for generating a strong yield response is Sargodha because it has the largest p-value, meaning it is the least statistically significant (Pr = 0.7138).
+#The environment that would be the worst choice for generating a strong yield response is Sargodha because it has the largest p-value, meaning it is the least statistically significant out of all the environments (p = 0.7138).
