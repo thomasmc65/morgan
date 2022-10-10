261fbbcaee59675b56cc60c48f38a6a6ce79967e
thomasmc65 <112492397+thomasmc65@users.noreply.github.com>
2022-10-10 14:36:12 -0400

    week 6

--- Week 6/Thomas_Week6_MixedModel_Assignment.R
+++ Week 6/Thomas_Week6_MixedModel_Assignment.R
@@ -16,28 +16,30 @@ df
 head(df)
 
 #Additive Effects
-glmm.mod1 <- glmmPQL(activity.level~claw.width + carapace.width + toadfish.cue.treatment, family = binomial, random = ~ 1 | ID, data = df)
+glmm.mod1 <- glmmPQL(activity.level~claw.width + carapace.width + toadfish.cue.treatment, family = binomial, random = ~ 1 | block, data = df)
 summary(glmm.mod1)
 
 #Interactive Effect
-glmm.mod2 <- glmmPQL(activity.level~claw.width + carapace.width * toadfish.cue.treatment, family = binomial, random = ~ 1 | ID, data = df)
+glmm.mod2 <- glmmPQL(activity.level~claw.width * carapace.width + toadfish.cue.treatment, family = binomial, random = ~ 1 | block, data = df)
 summary(glmm.mod2)
 
 # The authors used proportional consumption of prey as the (y) in their model, but did not include this in the dataset.
   # So we are going to create it - run the following line, assuming df= your data frame (feel free to change that):
-df$prop.cons <- df$eaten/df$prey 
+df$prop.cons <- df$eaten/df$prey
 
 # (Q1) - The code in line 8 is performing two operations at once. What are they? (2 pts)
 #The two operations the code in line 8 is performing are 
 
 # (Q2) - Did the interactive effect change which variables predict proportional consumption? How, specifically, did the results change? (5 pts)
-#The interactive effect did 
+#The interactive effect did not change which variables predict proportional consumption. For both of the additive and interactive effect models, toadfish cue treatments had the lowest p-value.
 
 # (Q3) - Plot the residuals of both models. Do you think either model is a good fit? Why or why not? (3 pts)
 plot(glmm.mod1$residuals, ylim = c(-.1,.1))
 plot(glmm.mod2$residuals, ylim = c(-.1,.1))
+#Neither model is a good fit because there is too obvious of a pattern in each residual plot. The data is not random enough.
 
 # Re-run both models as generalized additive models instead (using gam). Then compare the AIC of both models. (4 points each)
+
 #Additive Effects
 gam.mod1 <- gam(activity.level~claw.width + carapace.width + toadfish.cue.treatment, family = binomial, random = list(ID=~ 1), data = df)
 summary(gam.mod1)
