4a5267c6c7cc80d640a4ed2859334646bf20f4f4
thomasmc65 <112492397+thomasmc65@users.noreply.github.com>
2022-10-31 21:32:18 -0400

    week 9

--- Week 9/Thomas_Week9_Ecosystems_Assignment.R
+++ Week 9/Thomas_Week9_Ecosystems_Assignment.R
@@ -4,8 +4,6 @@
 # (Q1 - 12 pts) Use the dataset from the tutorial to complete one redundancy analysis (RDA) with variance partitioning on a different community (NOT the nematodes).
   # Explain the ecological importance of your significant predictor variables, or the importance if none are significant for your community.
 
-#For the invertebrate community, the predictor variables that will be used to complete an RDA with variance partitioning include 
-
 #do rda w/ different community
 
 library(readxl)
@@ -44,7 +42,10 @@ anova(ord)
 plot(ord, ylim = c(-2,2), xlim = c(-5,5))  
 ord <- rda(invert.means2 ~., abiotic.means2)
 
+#For the invertebrate community, the predictor variables used to complete the RDA with variance partitioning included pH, total nitrogen, percentage of ash rest, potassium, magnesium, calcium, aluminum, total phosphorous, and reactive phosphorous.
+#The variables most important for the invertebrate community were those related to soil conditions such as pH and nutrient abundance as these impact plant quality and thus the presence of invertebrates.
 #53% of the variance is explained by this redundancy analysis.
+#The significance of each of these predictor variables were further analyzed through the use of linear models below to find the best fit model.
 
 # (Q2 - 12 pts) Then use the dataset from the tutorial to create a linear model related to your RDA. Try multiple predictors to find the best fit model.
   # Explain the ecological importance of the significant predictors, or lack of significant predictors.
@@ -68,9 +69,12 @@ summary(mod3)$adj.r.squared #0.7566873
 
 plot(mod3$residuals)
 
-#From model 3, the interactive effect between total nitrogen and reactive phosphorous combined with the percentage of ash rest, calcium, aluminum, and total phosphorous a lower AIC when compared to the other two tested linear models.
-#These results make sense as nitrogen and phosphorous are limiting nutrients in an ecosystem.
+#From model 3, the interactive effect between total nitrogen and reactive phosphorous combined with the percentage of ash rest, calcium, aluminum, and total phosphorous had a lower AIC when compared to the other two tested linear models.
+#Model 1 was the global model that tested all predictor variables without any interactive effects. The least significant ones were removed in model 2. 
+#The same process was repeated after viewing the results for model 2, then different interactive effects between the variables were tested in model 3 to find the one with the lowest AIC value.
+#The results of model 3 make sense as nitrogen and phosphorous are limiting nutrients in an ecosystem.
 
 # (Q3 - 6 pts) Provide a 3-4 sentence synthesis of how these results relate to one another and the value of considering both together for interpreting biotic-abiotic interactions.
 
-#These results relate to one another in that 
+#These results relate to one another in that nitrogen and phosphorous are often limiting nutrients in ecosystems, meaning that the growth and reproduction of the invertebrate community is controlled by the availability and concentration of nitrogen and phosphorous.
+#This is a good example of how the abiotic and biotic components of an ecosystem interact and influence communities within that ecosystem. Analyzing the interactions of biotic and abiotic factors can therefore help with understanding ecosystems as a whole.
