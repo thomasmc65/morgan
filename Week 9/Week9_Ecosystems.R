05e8a719fa15f6ff5e1705e6111880a453ca143d
thomasmc65 <112492397+thomasmc65@users.noreply.github.com>
2022-11-28 22:18:39 -0500

    updates

--- Week 9/Week9_Ecosystems.R
+++ Week 9/Week9_Ecosystems.R
@@ -21,35 +21,6 @@ abiotic.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Abiotic factors
 # This can easily be fixed with our old friend as.data.frame:
 abiotic <- as.data.frame(abiotic.tibble)
 
----------------------------------------------------------------------------------
-
-#EXTRA EXAMPLE OF MERGING
-
-veg.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Abiotic factors")
-abiotic <- as.data.frame(abiotic.tibble)
-veg.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Vegetation_transects")
-veg <- as.data.frame(veg.tibble)
-view(abiotic)
-view(veg)
-
-?merge
-veg.abiotic <- merge(abiotic, veg, by = "Parcel", all = TRUE)
-view(veg.abiotic)
-
-?aggregate
-ab.means <- aggregate(x = abiotic, by = list(abiotic$Parcel), FUN = "mean")
-many2one <- merge(ab.means, veg, by.x = "Group.1", by.y = "Parcel", all = FALSE)
-
-
-merged <- merged[,-2]
-comp.merg <- complete.cases(merged)
-lm(length ~ pH, data = merged[,-4])
-              , data = merged, norm = TRUE)
-ord(merged)
-#Everything has to match in an ordination.
-
----------------------------------------------------------------------------------
-
 # First let's see what patterns might exist in the soil chemistry by land use or site:
 head(abiotic)
 boxplot(pH ~ Site, data= abiotic, main = "pH by Site")
