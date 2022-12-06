f1bec133a0ba4e09def6814cb34cd242a21a7783
thomasmc65 <112492397+thomasmc65@users.noreply.github.com>
2022-12-06 10:20:16 -0500

    final

--- Final Project/Thomas_Final_Project.R
+++ Final Project/Thomas_Final_Project.R
@@ -18,7 +18,43 @@ head(arthrain)
 df <- merge(soiltemp, arthrain, by = "period")
 df
 
-glm <- glm(Kthom~trt + avgmaxtemp + avgmintemp, family = poisson, data = df)
-glm
+glm.Kthom <- glm(Kthom~trt + avgmaxtemp + avgmintemp, family = poisson, data = df)
+summary(glm.Kthom)
 
-#What do I do about there being so many different sampled arthropod species? The above GLM is literally just one of them.
+glm.Ksalt <- glm(Ksalt~trt + avgmaxtemp + avgmintemp, family = poisson, data = df)
+summary(glm.Ksalt)
+
+glm.Ktitan <- glm(Ktitan~trt + avgmaxtemp * avgmintemp, family = poisson, data = df)
+summary(glm.Ktitan)
+
+#--------------------------------------------------------------------------------
+
+library(spdep)
+library(adespatial)
+library(vegan)
+  
+KY_Soil_Data.csv <- read.csv("KY_Soil_Data.csv", header=T)
+Wise_DH_Lensing_JR_2019.csv <- read.csv("Wise_DH_Lensing_JR_2019.csv", header=T)
+KY_Soil_Data.mat <- as.matrix(KY_Soil_Data.csv)
+Wise_DH_Lensing_JR_2019.mat <- as.matrix(Wise_DH_Lensing_JR_2019.csv)
+
+KY_Soil_Data.mat
+Wise_DH_Lensing_JR_2019.mat
+
+#Soil Temp Controlling Arthropods
+soiltemp.rda <- rda(df[,9:89]~avgmaxtemp + avgmintemp, data=df)
+soiltemp.rda
+anova(soiltemp.rda, perm.max = 10000)
+RsquareAdj(soiltemp.rda)
+
+#____ conditional, ____ constrained
+#RsquaredAdj = ____
+
+#Rainfall Controlling Arthropods
+rainfall.rda <- rda(df[,9:89]~trt, data=df)
+rainfall.rda
+anova(rainfall.rda, perm.max = 10000)
+RsquareAdj(rainfall.rda)
+
+#____ conditional, ____ constrained
+#RsquaredAdj = ____
